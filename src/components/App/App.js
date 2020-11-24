b8f038925b817b18d058980f4eeeffb12a2c7892
Garret Larson <garret.hs.larson@gmail.com>
2020-11-24 09:04:50 -0600

    Calendar fields are working and selector is pulling equipment names.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -52,6 +52,14 @@ class App extends Component {
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
             Even though it seems like they are different pages, the user is always on localhost:3000/user */}
+
+            <ProtectedRoute
+              // logged in shows UserPage else shows LoginPage
+              exact
+              path="/rental_submission_form"
+              component={SubmissionForm}
+            />
+
             <ProtectedRoute
               // logged in shows UserPage else shows LoginPage
               exact
