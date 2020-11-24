e0cc87ac608974400d86bddc1713e543605f6667
Sevisis Maverick <Mavericksevisis@gmail.com>
2020-11-24 14:21:09 -0600

    Add user modal (Finally) completed

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -17,8 +17,8 @@ if (process.env.DATABASE_URL) {
   const auth = params.auth.split(":");
 
   config = {
-    user: auth[0],
-    password: auth[1],
+    // user: auth[0],
+    // password: auth[1],
     host: params.hostname,
     port: params.port,
     database: params.pathname.split("/")[1],
--- src/components/_DR_Users/Users.jsx
+++ src/components/_DR_Users/Users.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
+import { Modal } from "react-bootstrap";
+import { Button, Table } from "react-bootstrap";
+import swal from "sweetalert";
+import { Row, Col } from "react-bootstrap";
 
 import UserItem from './UserItem';
 
 class Users extends Component {
   state = {
+    firstname: '',
+    lastname: '',
+    username: '',
+    phonenumber: '',
+    password: '',
+    super_admin: false,
     addUser: false,
   };
 
@@ -18,10 +28,40 @@ class Users extends Component {
     });
   };
 
+  resetState = () => this.setState({
+    firstname: '',
+    lastname: '',
+    username: '',
+    phonenumber: '',
+    password: '',
+    super_admin: false,
+    addUser: false,
+  });
+
+  onSubmit = () => {
+    this.props.dispatch({
+      type: 'REGISTER',
+      payload: {
+        firstname: this.state.firstname,
+        lastname: this.state.lastname,
+        username: this.state.username,
+        phonenumber: this.state.phonenumber,
+        password: this.state.password,
+        super_admin: this.state.super_admin,
+      },
+    })
+    this.resetState();
+    this.getUsers();
+  };
+
+  openAddUserModal = () => this.setState({ addUser: true });
+  closeAddUserModal = () => this.setState({ addUser: false });
+
   handleChange = (event) => {
     this.setState({
       [event.target.name]: event.target.value,
     });
+    console.log(this.state);
   };
 
   render() {
@@ -34,7 +74,34 @@ class Users extends Component {
 
         <br />
 
-        <button>Add User</button>
+        <button onClick={this.openAddUserModal}>Add User</button>
+
+        <Modal show={this.state.addUser} onHide={this.closeAddUserModal}>
+          <Modal.Header>
+            <Modal.Title>Add User</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            <input type='text' onChange={this.handleChange} name='firstname' placeholder='First Name' />
+            <input type='text' onChange={this.handleChange} name='lastname' placeholder='Last Name' />
+            <input type='text' onChange={this.handleChange} name='username' placeholder='Email' />
+            <input type='text' onChange={this.handleChange} name='phonenumber' placeholder='Phone Number' />
+            <input type='text' onChange={this.handleChange} name='password' placeholder='Password' />
+            <select onChange={this.handleChange} name='super_admin'>
+              <option>False</option>
+              <option>True</option>
+            </select>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <button onClick={this.resetState}>Cancel</button>
+            <button onClick={this.onSubmit}>Submit</button>
+          </Modal.Footer>
+        </Modal>
+
+
+
+
 
         <br />
 
@@ -53,7 +120,7 @@ class Users extends Component {
 
             {this.props.users !== undefined && this.props.users.map((user) => {
               return (
-                <UserItem user={user} key={user.id}/>
+                <UserItem user={user} key={user.id} />
               )
             })}
 
