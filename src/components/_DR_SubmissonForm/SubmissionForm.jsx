f43ce96190049f6cd6ba318902d733812551b584
Garret Larson <garret.hs.larson@gmail.com>
2020-11-25 11:31:55 -0600

    Added more validation to the form.

--- src/components/_DR_SubmissonForm/SubmissionForm.jsx
+++ src/components/_DR_SubmissonForm/SubmissionForm.jsx
@@ -21,35 +21,57 @@ class SubmissionForm extends Component {
     state: "",
     zip: "",
     email: "",
-    show: false
+    show: false,
+    formSubmissionSuccess: "",
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
     if (!(this.state.currentlySelectedEquipment)) {
       alert("Error: you cannot submit a form without selected equipment.");
+      return;
     }
     axios
       .post("/api/requests/", {
         company: this.state.practiceCompany,
         address: this.state.address,
-        point_of_contact: this.state.pointOfContact, 
-        email: this.state.email, 
-        phone_number: this.state.phoneNumber, 
-        city: this.state.city, 
-        state: this.state.state, 
-        zip: this.state.zip, 
-        start_date: this.state.startDate, 
-        end_date: this.state.endDate, 
-        purpose: this.state.purposeForRequest, 
+        point_of_contact: this.state.pointOfContact,
+        email: this.state.email,
+        phone_number: this.state.phoneNumber,
+        city: this.state.city,
+        state: this.state.state,
+        zip: this.state.zip,
+        start_date: this.state.startDate,
+        end_date: this.state.endDate,
+        purpose: this.state.purposeForRequest,
         equipment_in_request: this.state.currentlySelectedEquipment,
 
       })
       .then(() => {
-        console.log('Post successful.')
+        this.setState({
+          formSubmissionSuccess: true,
+          show: true,
+          pointOfContact: "",
+          city: "",
+          phoneNumber: "", //TODO: NEEDS TO BE NUMBER
+          practiceCompany: "",
+          address: "",
+          purposeForRequest: "",
+          availableEquipment: [], //an arr containing all equipment that is available for a given date range
+          arrOptions: [], //an arr that holds all available equipment that is valid for the react-select dropdown menu
+          currentlySelectedEquipment: "", //will be an array of equipment objects that have been selected by the user
+          state: "",
+          zip: "",
+          email: "",
+
+        });
+        console.log('Post successful.');
       })
-      .catch((err) => console.log(err));
-    
+      .catch((err) => {
+        console.log(err)
+        this.setState({ formSubmissionSuccess: false, show: true });
+      });
+
   }
 
   handleChange = (event) => {
@@ -59,7 +81,7 @@ class SubmissionForm extends Component {
   };
 
   handleSelect = (arrOfSelectedEquipment) => {
-    this.setState({currentlySelectedEquipment: arrOfSelectedEquipment})
+    this.setState({ currentlySelectedEquipment: arrOfSelectedEquipment })
   };
 
   handleDateChange = (dateParam, date) => {
@@ -73,7 +95,7 @@ class SubmissionForm extends Component {
       this.fetchAvailableEquipment(this.state.startDate, date); //due to asynchonous nature of setting state
       this.setState({ currentlySelectedEquipment: "" }); // if the user decides to change the date, we need to remove the currently selected equipment bc it is no longer valid!
     }
-    
+
   };
 
   fetchAvailableEquipment = (startDate, endDate) => {
@@ -87,7 +109,7 @@ class SubmissionForm extends Component {
         const options = res.data.map((equipmentObj) => {
           return { value: equipmentObj.id, label: equipmentObj.equipment_item, ...equipmentObj } //value allows us to select the correct equipment by using its id
         }); //value and label are keys that are required for the react-select dropdown menu
-        this.setState({ arrOptions: options}); //arrOptions needed for React-Select's Select tag
+        this.setState({ arrOptions: options }); //arrOptions needed for React-Select's Select tag
       })
       .catch((err) => console.log(err));
   };
@@ -95,92 +117,70 @@ class SubmissionForm extends Component {
   //bootstrap modal opening/closing handlers
   handleClose = () => this.setState({ show: false });
   handleShow = () => this.setState({ show: true });
-  renderModal = () => {
-    return (<Modal
-      show={this.state.show}
-      onHide={this.handleClose}
-      backdrop="static"
-      keyboard={false}
-    >
-      <Modal.Header closeButton>
-        <Modal.Title>Modal title</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        I will not close if you click outside me. Don't even try to press
-        escape key.
-        </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={this.handleClose}>
-          Close
-          </Button>
-        <Button variant="primary">Understood</Button>
-      </Modal.Footer>
-    </Modal>);
-  }
 
   render() {
-    return (    
+    return (
       <>
-        <Button onClick={this.handleShow}>Show modal</Button>
+        {/* <Button onClick={this.handleShow}>Show modal</Button> */}
         <h1>Dental Rental Request</h1>
 
         <Form onSubmit={this.handleSubmit}>
-        Start Date:
+          Start Date:
         <DatePicker required selected={this.state.startDate} onChange={(date) => this.handleDateChange("startDate", date)} />
-        {" "}
+          {" "}
         End Date:
         <DatePicker required selected={this.state.endDate} onChange={(date) => this.handleDateChange("endDate", date)} />
 
           <br />
           <br />
-        
-        <Select
-          isMulti
-          name="available-equipment"
-          options={this.state.arrOptions}
-          className="basic-multi-select"
-          classNamePrefix="select"
-          onChange={this.handleSelect}
-          value={this.state.currentlySelectedEquipment || ""} //this allows for validating date changes
+
+          <Select
+            isMulti
+            name="available-equipment"
+            options={this.state.arrOptions}
+            className="basic-multi-select"
+            classNamePrefix="select"
+            onChange={this.handleSelect}
+            value={this.state.currentlySelectedEquipment || ""} //this allows for validating date changes
           />
 
           <br />
 
-        <input
-          type="text"
-          name="pointOfContact"
-          placeholder="Full name"
-          value={this.state.pointOfContact}
-          onChange={this.handleChange}
-          required
-          />
-          
           <input
-          type="text"
-          name="practiceCompany"
-          placeholder="Practice/Company"
-          value={this.state.practiceCompany}
-          onChange={this.handleChange}
-          required
+            type="text"
+            name="pointOfContact"
+            placeholder="Full name"
+            value={this.state.pointOfContact}
+            onChange={this.handleChange}
+            required
           />
 
           <input
-          type="text"
-          name="purposeForRequest"
-          placeholder="Purpose for request"
-          value={this.state.purposeForRequest}
-          onChange={this.handleChange}
-          required
+            type="text"
+            name="practiceCompany"
+            placeholder="Practice/Company"
+            value={this.state.practiceCompany}
+            onChange={this.handleChange}
+            required
           />
 
-<br />
-        <input
-          type="number"
-          name="phoneNumber"
-          placeholder="Phone number"
-          value={this.state.phoneNumber}
-          onChange={this.handleChange}
-          required
+          <input
+            type="text"
+            name="purposeForRequest"
+            placeholder="Purpose for request"
+            value={this.state.purposeForRequest}
+            onChange={this.handleChange}
+            required
+          />
+
+          <br />
+          <input
+            type="number"
+            name="phoneNumber"
+            placeholder="Phone number"
+            value={this.state.phoneNumber}
+            onChange={this.handleChange}
+            required
           />
 
           <input
@@ -191,27 +191,25 @@ class SubmissionForm extends Component {
             onChange={this.handleChange}
             required
           />
-<br />
+          <br />
 
-        <input
-          type="text"
-          name="address"
-          placeholder="Address"
-          value={this.state.address}
-          onChange={this.handleChange}
-          required
-          />
-          
           <input
-          type="text"
-          name="city"
-          placeholder="City"
-          value={this.state.location}
-          onChange={this.handleChange}
-          required
-        />
+            type="text"
+            name="address"
+            placeholder="Address"
+            value={this.state.address}
+            onChange={this.handleChange}
+            required
+          />
 
-        
+          <input
+            type="text"
+            name="city"
+            placeholder="City"
+            value={this.state.city}
+            onChange={this.handleChange}
+            required
+          />
 
           <input
             type="text"
@@ -223,40 +221,56 @@ class SubmissionForm extends Component {
           />
 
           <input
-          type="number"
-          name="zip"
-          placeholder="Zip"
-          value={this.state.zip}
-          onChange={this.handleChange}
-          required
+            type="number"
+            name="zip"
+            placeholder="Zip"
+            value={this.state.zip}
+            onChange={this.handleChange}
+            required
           />
-          
+
           <br />
           <br />
 
           <Button type="submit">Submit Request</Button>
         </Form>
-        
-            <Modal
+
+        {/* Modal rendering when form submitted */}
+        {this.state.formSubmissionSuccess ? <Modal
           show={this.state.show}
           onHide={this.handleClose}
           backdrop="static"
           keyboard={false}
         >
-          <Modal.Header closeButton>
-            <Modal.Title>Modal title</Modal.Title>
+          <Modal.Header>
+            <Modal.Title>Successful Form Submission</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            I will not close if you click outside me. Don't even try to press
-            escape key.
-            </Modal.Body>
+            Thank you for submitting your Dental Rental request!
+             <br />
+            We will review your request and get back to you promptly.
+          </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={this.handleClose}>
-              Close
-              </Button>
-            <Button variant="primary">Understood</Button>
+            You will be redirected back to the ND Dental Foundation homepage in a few seconds.
+            </Modal.Footer>
+        </Modal> :
+          <Modal
+            show={this.state.show}
+            onHide={this.handleClose}
+            backdrop="static"
+            keyboard={false}
+          >
+            <Modal.Header>
+              <Modal.Title>Form Submission Error</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              Sorry - something went wrong!
+        </Modal.Body>
+            <Modal.Footer>
+              You will be redirected back to the ND Dental Foundation homepage in a few seconds.
           </Modal.Footer>
-        </Modal>
+          </Modal>}
+
       </>
     );
   }
