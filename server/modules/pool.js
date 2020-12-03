7e02ccb5f4df3088cfe1e2e9db87fd83e762ba21
Garret Larson <garret.hs.larson@gmail.com>
2020-12-03 15:30:06 -0600

    update comment pool

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -28,8 +28,8 @@ if (process.env.DATABASE_URL) {
   };
 } else {
   config = {
-    /*     user: "postgres",
-    password: "password", */
+    // user: "postgres",
+    // password: "password",
     host: "localhost", // Server hosting the postgres database
     port: 5432, // env var: PGPORT
     database: "dental_rental", // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
